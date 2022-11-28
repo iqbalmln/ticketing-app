d133663f77e301bf41135145bcbc07daebccea1f
iqbalmln <iqbalmaulana15784@gmail.com>
2022-11-28 22:26:10 +0700

    feat: implement route for app

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,35 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
+import Add from "../views/Add.vue"
+import TicketDetail from "../views/TicketDetail.vue"
+import Login from "../views/Login.vue"
+import Register from "../views/Register.vue"
 
 const routes = [
   {
     path: "/",
     name: "Home",
     component: Home,
+  },
+  {
+    path: "/add",
+    name: "Add",
+    component: Add
+  },
+  {
+    path: "/:id",
+    name: "TicketDetail",
+    component: TicketDetail
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: Login
+  },
+  {
+    path: "/register",
+    name: "Register",
+    component: Register
   },
 ];
 
